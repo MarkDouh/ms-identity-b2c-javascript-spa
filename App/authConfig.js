d8c9a3bda76da112dfe577809cdbd32406b4092a
MarkDouh <73522809+MarkDouh@users.noreply.github.com>
2022-01-14 15:38:46 +0100

    Update authConfig.js

--- App/authConfig.js
+++ App/authConfig.js
@@ -8,7 +8,7 @@
 
 const msalConfig = {
     auth: {
-      clientId: "2fdd06f3-7b34-49a3-a78b-0cf1dd87878e", // This is the ONLY mandatory field; everything else is optional.
+      clientId: "853945c7-2efd-4725-8f29-baddded79865", // This is the ONLY mandatory field; everything else is optional.
       authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose sign-up/sign-in user-flow as your default.
       knownAuthorities: [b2cPolicies.authorityDomain], // You must identify your tenant's domain as a known authority.
       redirectUri: "https://wa-b2c-mdot.azurewebsites.net", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
